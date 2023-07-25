5457778b3eff03182156b7ef95c1c429c1a10b51
Divyansh Mangal <divyanshmangal20@gmail.com>
2023-07-25 19:39:26 +0530

    Developed Succesfully

--- todo/urls.py
+++ todo/urls.py
@@ -4,6 +4,12 @@ from . import views
 urlpatterns = [
     #addtask
     path('addtask/',views.add_task,name='add_task'),
-    #completed_task
-    path('complete_task/<int:pk>',views.complete_task,name='complete_task')
+    #mark_as done__task
+    path('complete_task/<int:pk>/',views.complete_task,name='complete_task'),
+    #mark_as undone__task
+    path('uncomplete_task/<int:pk>/',views.uncomplete_task,name='uncomplete_task'),
+    #edit_task
+    path('edit_task/<int:pk>/',views.edit_task,name='edit_task'),
+    #delete_task
+    path('delete_task/<int:pk>/',views.delete_task,name='delete_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -12,4 +12,25 @@ def complete_task(request,pk):
     tasks.is_completed=True
     tasks.save()
     return redirect('home')
+def uncomplete_task(request,pk):
+    tasks=get_object_or_404(Task,pk=pk)
+    tasks.is_completed=False
+    tasks.save()
+    return redirect('home')
+def edit_task(request,pk):
+    gettasks=get_object_or_404(Task,pk=pk)
+    if request.method=="POST":
+        tasks=request.POST['task']
+        gettasks.task=tasks
+        gettasks.save()
+        return redirect('home')
+    else:
+        context={
+            'gettask':gettasks
+        }
+        return render(request,"edit_task.html",context)
+def delete_task(request,pk):
+    tasks=get_object_or_404(Task,pk=pk)
+    tasks.delete()
+    return redirect('home')
 
